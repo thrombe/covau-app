015246b338e0f62e1e0fd43ae8505343161e57f4
thrombe <yash.n.thombare620@gmail.com>
2024-05-26 23:21:07 +0530

    implement a bunch of stuff for LocalPlayer

--- electron/src/lib/local_player.ts
+++ electron/src/lib/local_player.ts
@@ -1,19 +1,66 @@
 import { YTNodes } from "./searcher/song_tube";
 import { new_innertube_instance } from "./searcher/tube";
-import type { PlayerMessage } from "$types/server";
+import type { PlayerMessage, PlayerCommand } from "$types/server";
 
+type MessageHandler = ((msg: PlayerMessage) => Promise<void>) | ((msg: PlayerMessage) => void);
 
 export class LocalPlayer {
 
     ws: WebSocket;
+    playing: string = '';
+    paused: boolean = false;
+    finished: boolean = false;
+    progress: number = 0.0;
+    duration: number = 0.0;
+    volume: number = 1.0;
+    listeners: Map<string, MessageHandler[]>;
 
     constructor() {
+        this.listeners = new Map();
         this.ws = new WebSocket("ws://localhost:10010/player");
         this.ws.addEventListener('message', async (e) => {
+            let message: PlayerMessage = JSON.parse(e.data);
+            console.log(message)
+
+            let handlers = this.listeners.get(message.type);
+            if (handlers) {
+                for (let handler of handlers) {
+                    handler(message);
+                }
+            }
+
+            switch (message.type) {
+                case 'Duration':
+                    this.duration = message.content;
+                    break;
+                case 'Paused':
+                    this.paused = true;
+                    break;
+                case 'Unpaused':
+                    this.paused = false;
+                    break;
+                case 'Playing':
+                    this.paused = false;
+                    this.finished = false;
+                    this.playing = message.content;
+                    break;
+                case 'ProgressPerc':
+                    this.progress = message.content;
+                    break;
+                case 'Volume':
+                    this.volume = message.content;
+                    break;
+                case 'Finished':
+                    this.finished = true;
+                    break;
+                default:
+                    console.warn("unhandled message: " + e.data);
+                    break;
+            }
         });
         this.ws.addEventListener('open', async (e) => {
             let p = await new_innertube_instance();
-            let res = await p.music.search("Milet", { type: 'song' });
+            let res = await p.music.search("Imagine dragons", { type: 'song' });
             console.log(res)
             let contents = res.contents!
                 .flatMap(e => e.contents?.filterType(YTNodes.MusicResponsiveListItem) ?? []);
@@ -27,13 +74,30 @@ export class LocalPlayer {
             let uri = f.decipher(p.session.player);
             console.log(url, url, f, uri)
 
-            let msg: PlayerMessage = { type: 'Play', content: uri };
-            this.ws.send(JSON.stringify(msg))
+            this.send_message({ type: 'Play', content: uri });
+            this.send_message({ type: 'SetVolume', content: 1.0 });
         });
     }
 
-    async play() {
+    set_message_listener(type: PlayerMessage['type'], handler: MessageHandler) {
+        let handlers = this.listeners.get(type);
+        if (handlers) {
+            handlers.push(handler);
+        } else {
+            this.listeners.set(type, [handler]);
+        }
+    }
 
+    send_message(msg: PlayerCommand) {
+        this.ws.send(JSON.stringify(msg));
+    }
+
+    play(uri: string | null = null) {
+        if (uri === null) {
+            this.send_message({ type: 'Unpause' });
+        } else {
+            this.send_message({type: 'Play', content: uri });
+        }
     }
 }
 
