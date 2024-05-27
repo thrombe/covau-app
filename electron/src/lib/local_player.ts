b27f1c9305c5b371dbb81a358d2a8093b42116e2
thrombe <yash.n.thombare620@gmail.com>
2024-05-27 17:25:01 +0530

    local player don't start playing on open

--- electron/src/lib/local_player.ts
+++ electron/src/lib/local_player.ts
@@ -58,23 +58,25 @@ export class LocalPlayer {
             }
         });
         this.ws.addEventListener('open', async (_e) => {
-            let p = await new_innertube_instance();
-            let res = await p.music.search("Imagine dragons", { type: 'song' });
-            console.log(res)
-            let contents = res.contents!
-                .flatMap(e => e.contents?.filterType(YTNodes.MusicResponsiveListItem) ?? []);
-            let v = contents[0];
-            console.log(v)
+            // let p = await new_innertube_instance();
+            // let res = await p.music.search("Imagine dragons", { type: 'song' });
+            // console.log(res)
+            // let contents = res.contents!
+            //     .flatMap(e => e.contents?.filterType(YTNodes.MusicResponsiveListItem) ?? []);
+            // let v = contents[0];
+            // console.log(v)
 
-            let d = await p.getInfo(v.id!);
-            console.log(d)
-            let f = d.chooseFormat({ type: 'audio', quality: 'best', format: 'opus', client: 'YTMUSIC_ANDROID' });
-            let url = d.getStreamingInfo();
-            let uri = f.decipher(p.session.player);
-            console.log(url, url, f, uri)
+            // let d = await p.getInfo(v.id!);
+            // console.log(d)
+            // let f = d.chooseFormat({ type: 'audio', quality: 'best', format: 'opus', client: 'YTMUSIC_ANDROID' });
+            // let url = d.getStreamingInfo();
+            // let uri = f.decipher(p.session.player);
+            // console.log(url, url, f, uri)
 
-            this.send_message({ type: 'Play', content: uri });
+            // this.send_message({ type: 'Play', content: uri });
+
             this.send_message({ type: 'SetVolume', content: 1.0 });
+            this.send_message({ type: 'Pause' });
         });
     }
 
