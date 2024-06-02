1c0c9a24f18ccfd7b9b5cf11e6d6568921fd8a40
thrombe <yash.n.thombare620@gmail.com>
2024-06-02 21:55:42 +0530

    local/VibeWrap no params

--- electron/src/lib/local/VibeWrap.svelte
+++ electron/src/lib/local/VibeWrap.svelte
@@ -2,12 +2,10 @@
     import { new_innertube_instance } from '$lib/searcher/tube.ts';
     import Loading from '$lib/components/Loading.svelte';
     import Vibe from './Vibe.svelte';
-
-    export let params: { group?: string };
 </script>
 
 {#await new_innertube_instance()}
     <Loading />
 {:then tube}
-    <Vibe bind:params {tube} />
+    <Vibe {tube} />
 {/await}
