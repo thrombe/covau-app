b36f045603d3d21544eae0e0f14c48f6465f9486
thrombe <yash.n.thombare620@gmail.com>
2024-06-06 21:58:50 +0530

    fmt

--- electron/src/lib/components/AudioListItem.svelte
+++ electron/src/lib/components/AudioListItem.svelte
@@ -12,7 +12,7 @@
     let hide_border = true;
 
     $: if (img_src || true) {
-        if (img_src == '') {
+        if (img_src == "") {
             hide_border = true;
         }
     }
@@ -27,12 +27,14 @@
     };
 </script>
 
-<item class='w-full h-full block relative py-1'>
-    <div class='w-full h-full pl-1 flex flex-row text-gray-200'>
-        <icon class='block p-1 aspect-square flex-none h-full'>
-            <div class='w-full h-full rounded-md overflow-hidden'>
+<item class="w-full h-full block relative py-1">
+    <div class="w-full h-full pl-1 flex flex-row text-gray-200">
+        <icon class="block p-1 aspect-square flex-none h-full">
+            <div class="w-full h-full rounded-md overflow-hidden">
                 <img
-                    class='w-full h-full object-cover {hide_border ? 'scale-150' : ''}'
+                    class="w-full h-full object-cover {hide_border
+                        ? 'scale-150'
+                        : ''}"
                     src={img_src}
                     draggable={false}
                     alt=""
@@ -42,12 +44,14 @@
             </div>
         </icon>
 
-        <info class='flex flex-col mx-2 overflow-hidden pt-1'>
-            <item-title class='flex flex-col justify-end h-1/2 text-sm'>
+        <info class="flex flex-col mx-2 overflow-hidden pt-1">
+            <item-title class="flex flex-col justify-end h-1/2 text-sm">
                 <txt>{item?.title() ?? ""}</txt>
             </item-title>
 
-            <item-title-sub class='flex flex-col justify-start h-1/2 text-xs text-gray-400'>
+            <item-title-sub
+                class="flex flex-col justify-start h-1/2 text-xs text-gray-400"
+            >
                 <txt>{item?.title_sub() ?? ""}</txt>
             </item-title-sub>
         </info>
@@ -55,34 +59,45 @@
 
     {#each options as option}
         {#if option.location == "TopRight"}
-            <button
-                class='pop-button top-0'
-                on:click={option.onclick}
-            >
-                <img alt="remove" draggable={false} class='h-3 opacity-50' src={option.icon}>
+            <button class="pop-button top-0" on:click={option.onclick}>
+                <img
+                    alt="remove"
+                    draggable={false}
+                    class="h-3 opacity-50"
+                    src={option.icon}
+                />
             </button>
         {:else if option.location == "IconTop"}
-            <div class='absolute h-full flex flex-col justify-center left-0 top-0'>
+            <div
+                class="absolute h-full flex flex-col justify-center left-0 top-0"
+            >
                 <button
-                    class='queue-button'
+                    class="queue-button"
                     class:play-button={true}
                     on:click={option.onclick}
                 >
-                    <img alt="play" draggable={false} class='scale-[50%]' src={option.icon}>
+                    <img
+                        alt="play"
+                        draggable={false}
+                        class="scale-[50%]"
+                        src={option.icon}
+                    />
                 </button>
             </div>
         {:else if option.location == "BottomRight"}
-            <button
-                class='pop-button bottom-0'
-                on:click={option.onclick}
-            >
-                <img alt="remove" draggable={false} class='h-3 opacity-50' src={option.icon}>
+            <button class="pop-button bottom-0" on:click={option.onclick}>
+                <img
+                    alt="remove"
+                    draggable={false}
+                    class="h-3 opacity-50"
+                    src={option.icon}
+                />
             </button>
         {/if}
     {/each}
 </item>
 
-<style lang='postcss'>
+<style lang="postcss">
     txt {
         @apply w-full text-ellipsis whitespace-nowrap overflow-hidden select-none;
     }
