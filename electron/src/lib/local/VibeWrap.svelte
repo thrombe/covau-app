40e32f2d4a92e87294f8e1259d1a8e500feb6c65
thrombe <yash.n.thombare620@gmail.com>
2024-06-11 23:11:46 +0530

    start server on frontend in VibeWrap

--- electron/src/lib/local/VibeWrap.svelte
+++ electron/src/lib/local/VibeWrap.svelte
@@ -4,12 +4,15 @@
     import Vibe from './Vibe.svelte';
     import { tube } from "$lib/stores.ts";
     import * as wasm from "$wasm/covau_app_wasm";
+    import { serve } from "$lib/server.ts";
 
     let promise = (async () => {
         let itube = await new_innertube_instance();
         tube.set(itube);
 
         let _info = await wasm.default();
+
+        let _ = serve();
     })()
 </script>
 
---
+++ electron/src/lib/server.ts
@@ -0,0 +1,39 @@
+import type { Message, Request } from '$types/server';
+import { toast } from './toast/toast';
+
+class Server {
+    ws: WebSocket;
+
+    constructor() {
+        this.ws = new WebSocket(`ws://localhost:${import.meta.env.SERVER_PORT}/serve`);
+
+        this.ws.addEventListener('message', async (e) => {
+            let mesg: Message<Request> = JSON.parse(e.data);
+            console.log(mesg)
+
+            if (mesg.type === "Err") {
+                toast(mesg.content, "error");
+                return;
+            }
+
+            let resp = await this.handle_req(mesg.content);
+
+            if (!resp) {
+                return;
+            }
+
+            let resp_mesg = { id: mesg.id, data: resp };
+
+            this.ws.send(JSON.stringify(resp_mesg));
+        });
+    }
+
+    async handle_req(req: Request): Promise<Object | null> {
+        return {};
+    }
+}
+
+export let server: Server | null = null;
+export const serve = async () => {
+    server = new Server();
+};
