601c6b9a37856243ca9cf61392954c2da2073560
thrombe <yash.n.thombare620@gmail.com>
2024-06-14 00:35:42 +0530

    update $types/

--- electron/src/types/covau.ts
+++ electron/src/types/covau.ts
@@ -1,8 +1,12 @@
 import type { ReleaseGroupWithInfo, ReleaseWithInfo, Recording } from '$types/mbz.ts';
 import type { Album, Video } from '$types/yt.ts';
 
+export type LocalState = { queue: Queue };
 export type PlaySource = { type: "File"; content: string } | { type: "YtId"; content: string };
-export type Song = { title: string; mbz_id: string | null; sources: PlaySource[] };
+export type InfoSource = { type: "YtId"; content: string } | { type: "MbzId"; content: string };
+export type Song = { haystacks: string[]; info_sources: InfoSource[]; play_sources: PlaySource[] };
+export type Playlist = { title: string; songs: Song[] };
+export type Queue = ListenQueue<Playlist>;
 export type UpdateItem<T> = { done: boolean; points: number; item: T };
 export type ListenQueue<T> = { queue: T; current_index: number };
 export type UpdateSource = { type: "Mbz"; content: { artist_id: string; release_groups: UpdateItem<ReleaseGroupWithInfo>[]; releases: UpdateItem<ReleaseWithInfo>[]; recordings: ListenQueue<UpdateItem<Recording>[]> } } | { type: "MusimanagerSearch"; content: { search_words: string[]; artist_keys: string[]; non_search_words: string[]; known_albums: UpdateItem<Album>[]; songs: ListenQueue<UpdateItem<Video>[]> } } | { type: "SongTubeSearch"; content: { search_words: string[]; artist_keys: string[]; known_albums: UpdateItem<Album>[]; songs: ListenQueue<UpdateItem<Video>[]> } };
