71b57778eae51bdf2e2db77fff38182a78f626ed
thrombe <yash.n.thombare620@gmail.com>
2024-06-13 23:32:40 +0530

    implement CustomListItem

--- electron/src/lib/searcher/item.ts
+++ electron/src/lib/searcher/item.ts
@@ -9,6 +9,49 @@ export abstract class ListItem {
     abstract audio_uri(): Promise<string | null>;
 }
 
+export class CustomListItem extends ListItem {
+    _key: string;
+    _title: string;
+    _title_sub: string | null = null;
+    _thumbnail: string | null = null;
+    _default_thumbnail: string = "/static/default-music-icon.svg"
+    _options: Option[] = [];
+
+    constructor(key: string, title: string) {
+        super();
+        this._key = key;
+        this._title = title;
+    }
+
+    key() {
+        return this._key;
+    }
+
+    title() {
+        return this._title;
+    }
+
+    title_sub() {
+        return this._title_sub;
+    }
+
+    thumbnail() {
+        return this._thumbnail;
+    }
+
+    default_thumbnail() {
+        return this._default_thumbnail;
+    }
+
+    async audio_uri() {
+        return null;
+    }
+
+    options(_ctx: RenderContext) {
+        return this._options;
+    }
+}
+
 export type RenderContext = "Queue" | "Browser" | "Playbar";
 export type Callback = (() => void) | (() => Promise<void>);
 export type Option = {
