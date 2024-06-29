fbb35806c9288811ff89a43b603d1197d40238fc
thrombe <yash.n.thombare620@gmail.com>
2024-06-30 03:55:49 +0530

    add $types/types.ts to export all types

---
+++ electron/src/types/types.ts
@@ -0,0 +1,9 @@
+
+import * as covau from "$types/covau.ts";
+import * as yt from "$types/yt.ts";
+import * as mm from "$types/musimanager.ts";
+import * as mbz from "$types/mbz.ts";
+import * as server from "$types/server.ts";
+import * as db from "$types/db.ts";
+
+export { covau, yt, mm, mbz, server, db };
