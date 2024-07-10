b21b3835f27a519cdd62843ad33933a5dc56b7fb
thrombe <yash.n.thombare620@gmail.com>
2024-07-10 23:17:44 +0530

    delete qt app in and use quick app instead

--- qweb/main.cpp
+++ qweb/main.cpp
@@ -1,42 +1,17 @@
-#include <QApplication>
-// #include <QQmlApplicationEngine>
-#include <QtWebEngineQuick/QtWebEngineQuick>
-#include <QtWebEngineWidgets/QWebEngineView>
-
-#include "main.h"
-#include "./ui_main.h"
-
-Covau::Covau(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::Covau)
-{
-    ui->setupUi(this);
-}
-
-Covau::~Covau()
-{
-    delete ui;
-}
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
 
 int main(int argc, char *argv[])
 {
-    // QtWebEngineQuick::initialize();
+    QGuiApplication app(argc, argv);
 
-    QApplication a(argc, argv);
+    QQmlApplicationEngine engine;
+    const QUrl url(u"qrc:/qweb/Main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
+        &app, []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.load(url);
 
-    // QQmlApplicationEngine engine;
-    // engine.load(QUrl("qrc:/main.qml"));
-
-    Covau w;
-    auto parent = w.centralWidget();
-    // auto parent = w.parentWidget();
-
-    QWebEngineView *view = new QWebEngineView(parent);
-    view->load(QUrl("http://localhost:6175/#/local/"));
-    view->show();
-
-    w.show();
-
-    return a.exec();
+    return app.exec();
 }
