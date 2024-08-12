85df582b270e50718ca49bfe4d9de3d8a88c1904
thrombe <yash.n.thombare620@gmail.com>
2024-08-12 22:24:02 +0530

    impl RefStore for db items (refrence counting)

---
+++ electron/src/lib/rc.ts
@@ -0,0 +1,127 @@
+import * as types from "$types/types.ts";
+import * as utils from './utils.ts';
+import * as server from './server.ts';
+
+class RefStore<T extends types.db.DbItem<unknown>> {
+    store = new Map<number, RcItem<T>>();
+    registry = new FinalizationRegistry((id: number) => {
+        let rs = this.store.get(id) ?? null;
+        if (rs == null) {
+            throw new Error(`item with id ${id} is not in store`);
+        }
+
+        rs.free();
+    });
+
+    rc(t: T) {
+        let k = this.store.get(t.id) ?? null;
+        if (k != null) {
+            if (k.t.metadata.update_counter >= t.metadata.update_counter) {
+                return k.rc();
+            } else {
+                k.t = t;
+                return k.rc();
+            }
+        }
+
+        let rs = new RcItem(t);
+        this.store.set(t.id, rs);
+        return rs.rc();
+    }
+}
+let store = new RefStore();
+
+// refrence store
+class RcItem<T extends types.db.DbItem<unknown>> {
+    _t: Readonly<T>;
+    count: number;
+    constructor(t: T) {
+        t = utils.deep_freeze(t);
+        this._t = t;
+        this.count = 0;
+    }
+
+    get t() {
+        return this._t;
+    }
+
+    set t(t: T) {
+        t = utils.deep_freeze(t);
+        this._t = t;
+    }
+
+    free() {
+        this.count -= 1;
+        if (this.count > 0) {
+            return false;
+        }
+
+        if (!store.store.delete(this.t.id)) {
+            throw new Error(`item with id ${this.t.id} is not in store`);
+        }
+
+        // console.log("delete", this.t.id, this.t);
+        return true;
+    }
+
+    rc() {
+        this.count += 1;
+        let r = new Rc<T>(this.t.id);
+        store.registry.register(r, this.t.id, r);
+        // console.log("rc", this.t.id, this.count, store.store.size);
+        return r;
+    }
+}
+
+// refrence counter
+export class Rc<T extends types.db.DbItem<unknown>> {
+    id: number;
+    constructor(id: number) {
+        this.id = id;
+    }
+
+    get t(): Readonly<T> {
+        let item = store.store.get(this.id) ?? null;
+        if (item == null) {
+            throw new Error(`item with id ${this.id} is not in store`);
+        }
+        return item.t as T;
+    }
+
+    set t(t: T) {
+        if (this.id != t.id) {
+            throw new Error(`attempt to assign ${t.id} in Rc for ${this.id}`);
+        }
+        let item = store.store.get(this.id) ?? null;
+        if (item == null) {
+            throw new Error(`item with id ${this.id} is not in store`);
+        }
+        if (item.t.metadata.update_counter >= t.metadata.update_counter) {
+            throw new Error("new update_counter must be greater than old update_counter for the DbItem");
+        }
+        item.t = t;
+    }
+
+    cloned() {
+        let t = this.t;
+        return structuredClone(t);
+    }
+
+    async txn(fn: (t: T) => Promise<T>, dbops: server.DbOps | null = null) {
+        let t = this.cloned();
+        t = await fn(t);
+        if (dbops == null) {
+            t = await server.db.txn(async dbops => {
+                return await dbops.update(t);
+            }) as T;
+        } else {
+            t = await dbops.update(t) as T;
+        }
+        this.t = t;
+    }
+}
+
+export const rc = {
+    store,
+};
+
