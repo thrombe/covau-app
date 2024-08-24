19623249da59b0859d704c4876492e265801fb23
thrombe <yash.n.thombare620@gmail.com>
2024-08-25 03:28:32 +0530

    install windows libs for webui

--- flake.nix
+++ flake.nix
@@ -42,14 +42,55 @@
       # - [Cross Compile Rust for Windows - Help - NixOS Discourse](https://discourse.nixos.org/t/cross-compile-rust-for-windows/9582/7)
       # windows-pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux.pkgsCross.mingw32;
       # windows-pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux.pkgsCross.mingwW64;
-      windows-pkgs = import inputs.nixpkgs {
-        localSystem = "x86_64-linux";
-        crossSystem.config = "x86_64-w64-mingw32";
-      };
+      # windows-pkgs = import inputs.nixpkgs {
+      #   localSystem = "x86_64-linux";
+      #   crossSystem.config = "x86_64-w64-mingw32";
+      # };
       # windows-pkgs = import inputs.nixpkgs {
       #   localSystem = "x86_64-linux";
       #   crossSystem.config = "x86_64-w64-mingwW64";
       # };
+      windows-pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux.pkgsCross.mingwW64;
+
+      # - [fatal error: EventToken.h: No such file or directory](https://github.com/webview/webview/issues/1036)
+      # - [MinGW-w64 requirements](https://github.com/webview/webview?tab=readme-ov-file#mingw-w64-requirements)
+      # - [WinLibs - GCC+MinGW-w64 compiler for Windows](https://winlibs.com/#download-release)
+      winlibs = windows-pkgs.stdenv.mkDerivation {
+        name = "winlibs";
+        src = windows-pkgs.fetchzip {
+          url = "https://github.com/brechtsanders/winlibs_mingw/releases/download/14.2.0posix-18.1.8-12.0.0-ucrt-r1/winlibs-x86_64-posix-seh-gcc-14.2.0-llvm-18.1.8-mingw-w64ucrt-12.0.0-r1.zip";
+          sha256 = "sha256-xBRZ8NJmWXpvraaTpXBkd2QbhF5hR/8g/UBPwCd12hc=";
+        };
+
+        phases = ["installPhase"];
+        installPhase = ''
+          mkdir $out
+          cp -r $src/* $out/.
+        '';
+      };
+      mcfgthread = windows-pkgs.stdenv.mkDerivation {
+        name = "mcfgthread";
+        src = windows-pkgs.fetchurl {
+          url = "https://mirror.msys2.org/mingw/mingw64/mingw-w64-x86_64-mcfgthread-1.8.3-1-any.pkg.tar.zst";
+          sha256 = "sha256-ogfmo9utCtE2WpWtmPDuf+M6WIvpp1Xvxn+aqRu+nbs=";
+        };
+
+        nativeBuildInputs = [
+          pkgs.zstd
+        ];
+
+        phases = ["installPhase"];
+        installPhase = ''
+          mkdir $out
+          cp $src $out/src
+          cd $out
+
+          tar --zstd -xvf src
+          rm src
+          mv mingw64/* .
+          rmdir mingw64
+        '';
+      };
       rust-bin = inputs.rust-overlay.lib.mkRustBin { } windows-pkgs.buildPackages;
       windows-webui = windows-pkgs.stdenv.mkDerivation {
         name = "webui";
@@ -92,6 +133,8 @@
           rust-bin.stable.latest.minimal
           windows-pkgs.buildPackages.pkg-config
           windows-pkgs.openssl
+          winlibs
+          mcfgthread
         ];
 
         depsBuildBuild = [];
@@ -102,6 +145,8 @@
           windows-pkgs.windows.pthreads
           windows-webui
           windows-mpv
+          winlibs
+          mcfgthread
         ];
 
         env = {
@@ -219,7 +264,7 @@
           webui
           qweb
 
-          # gst_all_1.gstreamer
+          gst_all_1.gstreamer
           # gst_all_1.gst-plugins-base
           # gst_all_1.gst-plugins-good
           # gst_all_1.gst-plugins-bad
