a911992c92189868b7025a97824613deecaba47f
thrombe <yash.n.thombare620@gmail.com>
2024-09-15 19:15:16 +0530

    remove unused code from MainActivity.kt

--- android/app/src/main/java/com/thrombe/covau/MainActivity.kt
+++ android/app/src/main/java/com/thrombe/covau/MainActivity.kt
@@ -10,17 +10,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.thrombe.covau.ui.theme.CovauTheme
 
@@ -34,6 +30,9 @@ class MainActivity : ComponentActivity() {
         }
         thread.start()
 
+        // TODO: do this edge to edge thing. i.e, render behind the status bar and the navigation bar.
+        // somehow need to reserve space for both in html
+        // enableEdgeToEdge()
         setContent {
             MaterialTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
@@ -41,18 +40,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-
-//        enableEdgeToEdge()
-//        setContent {
-//            CovauTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-//            }
-//        }
     }
 }
 
@@ -62,11 +49,9 @@ fun WebViewScreen() {
     val context = LocalContext.current
     // TODO: fetch this url from rust.
     val url = "http://localhost:6176/#/local"
-//    val url = "https://covau.netlify.app/"
-//    val url = "https://youtube.com/"
-//    val url = "chrome://inspect/"
+    // val url = "https://covau.netlify.app/"
+    // val url = "https://youtube.com/"
 
-    // Create a WebView inside the AndroidView composable
     AndroidView(
         factory = {
             WebView(context).apply {
@@ -77,82 +62,12 @@ fun WebViewScreen() {
                     }
                 }
 
-                settings.javaScriptEnabled = true // Enable JavaScript if needed
-                settings.allowContentAccess = true
-                settings.blockNetworkImage = false
-                settings.blockNetworkLoads = false
-                settings.safeBrowsingEnabled = false
-                settings.allowFileAccess = true
-                settings.allowUniversalAccessFromFileURLs = true
-                settings.allowFileAccessFromFileURLs = true
-                settings.userAgentString = "${settings.userAgentString} wasm"
-                loadUrl(url) // Load the specified URL
+                settings.javaScriptEnabled = true
+                loadUrl(url)
             }
         },
-        update = { webview ->
-            webview.loadUrl(url) // Load the URL when the view is updated
-
-
-            val htmlContent = """
-                    <!DOCTYPE html>
-                    <html>
-                    <head>
-                        <style>
-                            html, body {
-                                margin: 0;
-                                padding: 0;
-                                height: 100vh;
-                                width: 100vw;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <div style="height: 100%; background-color: lightgreen;">This is a WebView!</div>
-                    </body>
-                        <script>
-                            //setTimeout(() => {
-                            console.log(window.innerHeight);
-                            let el = document.getElementsByTagName('body')[0];
-                            //el.style.height = `${'$'}{window.innerHeight}px`;
-                            //}, 2000)
-                        </script>
-                    </html>
-                """.trimIndent()
-
-//            webview.loadDataWithBaseURL(null, htmlContent, "text/html", "UTF-8", null)
-
-            // Inject JavaScript to set the height of the WebView based on content
-//            webview.evaluateJavascript(
-//                """
-//                (function() {
-//                    console.log(window.innerHeight);
-//                    var height = document.body.scrollHeight;
-//                     Android.setWebViewHeight(height);
-//                     var el = document.getElementsByTagName('body')[0];
-//                     el.style.height = "${'$'}{Dimensions.get('window').height}px';
-//                })();
-//                """.trimIndent(),
-//                null
-//            )
-
+        update = { webView ->
+            webView.loadUrl(url) // Load the URL when the view is updated
         },
-//        modifier = Modifier.fillMaxSize()
-//        modifier = Modifier.size(30.dp)
     )
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CovauTheme {
-        Greeting("Android")
-    }
 }
